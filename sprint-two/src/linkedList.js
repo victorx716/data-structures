29b5566fdf7d5ea4e4c757cb01e2c4565694a237
victorx716 <victorliu716@gmail.com>
2019-01-05 14:26:02 -0800

    Added some code for first linked list method

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,36 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-
-  list.addToTail = function(value) {
+  
+  list.addToTail = function(value) { 
+    if (list.head === null) {
+    list.head = Node(value);
+    list.tail = list.head;
+    } else {
+      list.head.next = Node(value);
+      list.tail = list.head.next;
+    }
+    
+    //.addToTail() method, takes a value and adds it to the end of the list
+    // current list.tail -> value;
+    //list.tail = value; 
+    //value.list = null;
+    //value.tail = tail;
+    
   };
 
   list.removeHead = function() {
+    
+    list.head = list.tail;
+    
   };
 
   list.contains = function(target) {
+    //variable for if target was found
+    //base case of if list.head.next === target 
+    //else variable === false
+
+    //
   };
 
   return list;
