d6563ee5ab2ab80f283e5a7084c239fab1b16622
Victor Liu <victorliu716@gmail.com>
2019-01-06 14:36:26 -0800

    Fixed linked list, added code for tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,50 @@
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-
-  list.addToTail = function(value) {
+  
+  list.addToTail = function(value) { 
+    if (list.head === null) {
+      list.head = Node(value);
+      list.tail = list.head;
+    } else {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
+    
+    //.addToTail() method, takes a value and adds it to the end of the list
+    // current list.tail -> value;
+    //list.tail = value; 
+    //value.list = null;
+    //value.tail = tail;
   };
 
   list.removeHead = function() {
+    let temp = list.head.value;
+
+    list.head = list.head.next;
+    
+    return temp;
   };
 
   list.contains = function(target) {
+    //variable for if target was found
+    //base case of if list.head.next === target 
+    //else variable === false
+    // let isTrue;
+    let loc = list.head;
+
+    let traverse = function(loc, target) {
+      if (loc.value === target) {
+        return true;
+      } else if (loc.next === null) {
+        return false;
+      } else {
+        return traverse(loc.next, target);
+      }
+    };
+    return traverse(loc, target);
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,38 @@
 var Tree = function(value) {
   var newTree = {};
+
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; 
+  _.extend(newTree, treeMethods);
+  
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value))
 };
 
 treeMethods.contains = function(target) {
+  let doesContain = function (node, target) {
+    if (node.value === target) {
+      return true;
+    }
+    
+    for (let i = 0; i < node.children.length; i++) {
+      if (doesContain(node.children[i], target)){
+        return true;
+      }
+    }
+
+    return false;
+  }
+  return doesContain(this, target);
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addChild - O(1)
+ contains - best case - O(1), worst case - O(n)
  */
