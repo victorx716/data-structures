ceb00ed7d1eddebe19feeb77f74734b5627f637c
victorx716 <victorliu716@gmail.com>
2019-01-05 16:31:43 -0800

    Submitted implementation of linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,11 +5,11 @@ var LinkedList = function() {
   
   list.addToTail = function(value) { 
     if (list.head === null) {
-    list.head = Node(value);
-    list.tail = list.head;
+      list.head = Node(value);
+      list.tail = list.head;
     } else {
-      list.head.next = Node(value);
-      list.tail = list.head.next;
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
     }
     
     //.addToTail() method, takes a value and adds it to the end of the list
@@ -17,21 +17,33 @@ var LinkedList = function() {
     //list.tail = value; 
     //value.list = null;
     //value.tail = tail;
-    
   };
 
   list.removeHead = function() {
-    
+    let temp = list.head.value;
+
     list.head = list.tail;
     
+    return temp
   };
 
   list.contains = function(target) {
     //variable for if target was found
     //base case of if list.head.next === target 
     //else variable === false
+    // let isTrue;
+    let loc = list.head;
 
-    //
+    let traverse = function(loc, target) {
+      if (loc.value === target) {
+        return true;
+      } else if (loc.next === null) {
+          return false;
+      } else {
+          return traverse(loc.next, target)
+      }
+    }
+    return traverse(loc, target);
   };
 
   return list;
